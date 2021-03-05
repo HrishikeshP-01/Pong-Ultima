4dd9a775ecc10e90c81efb9747bd4d62abdbf1cd
Hrishikesh <hrishikeshpramodnair@gmail.com>
2021-03-06 00:29:19 +0530

    Cleaned up the single player pong code, added a header file

---
+++ Game.h
@@ -0,0 +1,43 @@
+#pragma once
+# include "SDL.h";
+
+// Vector2 struct that stores the co-ordinates
+struct Vector2 {
+	float x;
+	float y;
+};
+
+// Game class
+class Game {
+public:
+	Game();
+	// Initialize the game
+	bool Initialize();
+	// Runs the game loop until the game is over
+	void RunLoop();
+	// Shutdown the game
+	void Shutdown();
+private:
+	// Helper functions for the game loop
+	void ProcessInput();
+	void UpdateGame();
+	void GenerateOutput();
+	// Window created by SDL
+	SDL_Window* mWindow;
+	// Renderer for 2D drawing
+	SDL_Renderer* mRenderer;
+	// Number of ticks since start of game
+	Uint32 mTicksCount;
+	// Game should continue to run
+	bool mIsRunning;
+
+	// Pong specific 
+	// Direction of paddle
+	int mPaddleDir;
+	// Position of paddle
+	Vector2 mPaddlePos;
+	// Position of ball
+	Vector2 mBallPos;
+	// Velocity of ball
+	Vector2 mBallVel;
+};
